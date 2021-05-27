2ed20adaab822fd8ac6eb08210d676981711b81a
tduong <duongt.sbc@gmail.com>
2021-05-26 20:05:42 -0700

    database loading from csv working

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import csv
+import sqlite3
+import argparse
+
+def csv_getData(path: str):
+    with open (path) as csv_f:
+        csv_r = csv.reader(csv_f, delimiter=',')
+        csv_data = [row for row in csv_r]
+        print(csv_data[0])
+    return csv_data
+
+def insert_db(cur: sqlite3.Cursor, table: str, data):
+    query = ("INSERT INTO %s VALUES (NULL" % table) + ", ?"*len(data[0]) + ")"
+    print(query)
+    cur.executemany(query, data)
+
+def main(args):
+    print("connecting to: " + args.database_path)
+    db = sqlite3.connect(args.database_path)
+    if not db:
+        print("Could not connect to database")
+        exit
+    cur = db.cursor()
+    if not cur:
+        print("Could not create cursor")
+        exit
+    csv_data = csv_getData(args.csv_path)   # csv needs to be loaded into an array before we can do anything with it
+    insert_db(cur, args.table_name, csv_data[1:])
+    db.commit()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="update a database from a CSV")
+    parser.add_argument(
+            "--database", "-d", dest="database_path", required=True, 
+            help="The database uri")
+    parser.add_argument(
+            "--csv", "-c", dest="csv_path", required=True, 
+            help="The csv uri")
+    parser.add_argument(
+            "--table", "-t", dest="table_name", required=True, 
+            help="The table name")
+    args = parser.parse_args()
+    main(args)
